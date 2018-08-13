d12dc9dd7415cab1e0b8cb4a6a4c4b0686b165f2
himanshup <himanshup@csu.fullerton.edu>
2018-08-13 02:35:41 -0700

    rm cached some files

--- node_modules/mongoose/lib/drivers/node-mongodb-native/index.js
+++
@@ -1,9 +0,0 @@
-/*!
- * Module exports.
- */
-
-exports.Binary = require('./binary');
-exports.Collection = require('./collection');
-exports.Decimal128 = require('./decimal128');
-exports.ObjectId = require('./objectid');
-exports.ReadPreference = require('./ReadPreference');
